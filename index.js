eaf075aad01d661ec898e433ba3c2e8c245e68c8
Johan <ju.valdez24@info.uas.edu.mx>
2026-05-27 07:50:04 -0700

    corregido

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import sequelize from './database.js';
-import Pelicula from './Pelicula.js';
-
+import sequelize from './database.js'; 
+import Pelicula from './pelicula.js';  
+ 
 const app = express();
 app.use(express.json());
 
